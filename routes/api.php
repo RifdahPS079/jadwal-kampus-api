cb4b3d1ff57e27f68e7a24faf3b4dc44a6e6482a
RifdahPS079 <rifdahpritama09@gmail.com>
2026-04-29 07:26:05 +0800

    add reset password route

--- routes/api.php
+++ routes/api.php
@@ -142,3 +142,16 @@ Route::prefix('mahasiswa')
 
  Route::post('/save-fcm-token', [AuthController::class, 'saveFcmToken'])
     ->middleware('auth:dosen,mahasiswa');
+
+Route::get('/reset-dosen', function () {
+    $user = \App\Models\Dosen::where('email','putriayu@gmail.com')->first();
+
+    if (!$user) {
+        return "User tidak ditemukan";
+    }
+
+    $user->password = \Hash::make('123456');
+    $user->save();
+
+    return "Password reset berhasil";
+});
